aea08e4bd0ff2a17e96ce8ef4cb7ce0be16ebbad
Nitinjr812 <nitinjr812@gmail.com>
2026-05-18 15:28:15 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend working 🚀"
+  });
+});
+
+app.get("/api/data", (req, res) => {
+  res.json({
+    name: "Baby",
+    status: "Connected frontend + backend"
+  });
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
