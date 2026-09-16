12f34c015fca02dc8b15f34717942709fed1f7eb
Nitinjr812 <nitinjr812@gmail.com>
2026-09-16 10:56:46 +0530

    better speed

--- create-superadmin.js
+++ create-superadmin.js
@@ -6,8 +6,8 @@ const SuperAdmin = require("./models/SuperAdmin");
 async function run() {
   await mongoose.connect(process.env.MONGO_URI);
 
-  const email = "devnity10@gmail.com";        
-  const password = "strongnitin!1010";       
+  const email = "devnity10@gmail.com";       // ⬅️ apna email daal
+  const password = "draftbillsuperadmin123";       // ⬅️ apna strong password daal
 
   const existing = await SuperAdmin.findOne({ email });
   if (existing) {
